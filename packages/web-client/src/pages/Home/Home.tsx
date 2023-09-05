494500f8b8bc660044482d6c770f509a3a4455c5
ChetanRath <chetanrathore687@gmail.com>
2023-09-05 14:23:39 +0530

    added a form

--- packages/web-client/src/pages/Home/Home.tsx
+++ packages/web-client/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
+import { Grid } from "@mui/material";
 import { EMAIL_VALIDATION_REGEX } from "utils/types/common/constants";
 
 import { FormTextField } from "components/atoms/FormTextField";
@@ -16,6 +17,7 @@ export const Home = () => {
   } = form;
   return (
     <div>
+      <h1> User details </h1>
       <FormProvider {...form}>
         <form
           onSubmit={e => {
@@ -23,16 +25,45 @@ export const Home = () => {
             // handleSubmit(onSubmit)();
           }}
         >
-          <FormTextField
-            {...register( "email", {
-              required: "Email is required",
-              pattern: {
-                value: EMAIL_VALIDATION_REGEX,
-                message: "Invalid email address",
-              },
-            })}
-            textFieldProps={{ placeholder: "Enter your email" }}
-          />
+          <Grid container>
+            <Grid item xs={12} md={6}>
+              <FormTextField
+                {...register( "firstName", {
+                  required: "First Name is required",
+                })}
+                textFieldProps={{ placeholder: "Your First Name" }}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <FormTextField
+                {...register( "lastName", {
+                  required: "Last Name is required",
+                })}
+                textFieldProps={{ placeholder: "Your Last Name" }}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <FormTextField
+                {...register( "email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: EMAIL_VALIDATION_REGEX,
+                    message: "Invalid email address",
+                  },
+                })}
+                textFieldProps={{ placeholder: "Enter your email" }}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <FormTextField {...register( "nickname" )} textFieldProps={{ placeholder: "Your nickname" }} />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <FormTextField {...register( "address" )} textFieldProps={{ placeholder: "Enter your address" }} />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <FormTextField {...register( "phoneNumber" )} textFieldProps={{ placeholder: "Enter your Phone No." }} />
+            </Grid>
+          </Grid>
         </form>
       </FormProvider>
     </div>
